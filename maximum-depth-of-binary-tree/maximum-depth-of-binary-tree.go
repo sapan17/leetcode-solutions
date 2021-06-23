c86bf04e110afd9da68b07bdd0b3d6c82e51f266
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-06-23 00:05:49 -0600

    Time: 4 ms (93.13%), Space: 4.3 MB (76.88%) - LeetHub

---
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+    if (root == nil){
+        return 0
+    }
+    
+    left:= maxDepth(root.Left)+1
+    right:= maxDepth(root.Right)+1
+    
+    if left > right{
+        return left
+    } else{
+        return right
+    }
+}
