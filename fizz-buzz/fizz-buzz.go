0b22091d31e8256eda909b35a17dc837e629817b
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-06-23 00:31:04 -0600

    Time: 4 ms (96.72%), Space: 4.2 MB (23.72%) - LeetHub

---
+++ fizz-buzz/fizz-buzz.go
@@ -0,0 +1,19 @@
+func fizzBuzz(n int) []string {
+   var res []string
+    for i :=1; i<=n; i++{
+        if i%3 == 0 && i%5 == 0{
+            res = append(res,"FizzBuzz")
+            continue
+        }
+        if i%5 == 0{
+            res = append(res,"Buzz")
+            continue
+        }
+        if i%3 == 0{
+            res = append(res,"Fizz")
+            continue
+        }
+        res = append(res,strconv.Itoa(i))
+    }
+    return res
+}
