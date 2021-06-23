cba57bee03b7ae4e2a4a2b08c00fd0c8253e8c35
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-06-23 00:13:24 -0600

    Time: 0 ms (100.00%), Space: 3.9 MB (14.29%) - LeetHub

---
+++ shuffle-the-array/shuffle-the-array.go
@@ -0,0 +1,8 @@
+func shuffle(nums []int, n int) []int {
+    result := []int{}
+    for i := 0; i<n; i++{
+        result = append(result,nums[i])
+        result = append(result,nums[i+n])
+    }
+    return result
+}
