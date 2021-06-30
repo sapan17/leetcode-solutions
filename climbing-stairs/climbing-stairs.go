fc5764528f762baa9c53aac7eec031f9e1447cd1
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-06-30 13:24:55 -0700

    Time: 0 ms (100.00%), Space: 1.9 MB (64.27%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.go
@@ -0,0 +1,10 @@
+func climbStairs(n int) int {
+    if n < 3 {
+        return n
+    }
+    prev, curr := 1, 2
+    for i:= 3; i<=n; i++ {
+        prev, curr = curr, prev+curr
+    }
+    return curr
+}
