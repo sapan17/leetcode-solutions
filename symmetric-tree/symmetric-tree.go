46fbd932306ae4d0847f5198b0dc7c375fe3fc3c
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-06-30 13:06:23 -0700

    Time: 0 ms (100.00%), Space: 2.9 MB (31.21%) - LeetHub

---
+++ symmetric-tree/symmetric-tree.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+    if root == nil{
+        return false
+    }
+    if root.Left == nil && root.Right == nil{
+        return true
+    }
+    if root.Left == nil{
+        return false
+    }
+    if root.Right == nil{
+        return false
+    }
+    if root.Left.Val != root.Right.Val{
+        return false
+    }
+    return isSymmetric(&TreeNode{Left:root.Left.Left, Right:root.Right.Right}) && isSymmetric(&TreeNode{Left:root.Left.Right, Right: root.Right.Left})
+}
