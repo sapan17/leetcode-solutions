c9d831a7afc7ca198ad661c273b0561a98d7290c
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-08-29 04:11:15 +0530

    Time: 4 ms (94.27%), Space: 3.2 MB (35.49%) - LeetHub

--- maximum-subarray/maximum-subarray.go
+++ maximum-subarray/maximum-subarray.go
@@ -2,15 +2,19 @@ func maxSubArray(nums []int) int {
     if len(nums) == 1{
         return nums[0]
     }
-    max := nums[0]
-    for i:=1;i<len(nums);i++{
-        if nums[i]+nums[i-1] > nums[i]{
-            nums[i] = nums[i] + nums[i-1]
-        }
-        if nums[i] > max{
-            max = nums[i]
-        }
+    maxi := nums[0]
+    curr := nums[0]
+    for i:=1; i<len(nums);i++{
+        curr = max(nums[i]+curr, nums[i])
+        maxi = max(curr, maxi)
     }
-    return max
-    
+    return maxi
+}
+
+func max(a,b int)int{
+    if a > b{
+        return a
+    } else{
+        return b
+    }
 }
