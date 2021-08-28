b6b51db5c09f15a031e5d41025b0493194a62d13
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-08-29 03:49:27 +0530

    Time: 16 ms (94.13%), Space: 6.2 MB (85.60%) - LeetHub

--- contains-duplicate/contains-duplicate.go
+++ contains-duplicate/contains-duplicate.go
@@ -1,13 +1,10 @@
 func containsDuplicate(nums []int) bool {
-    
-    m := make(map[int]bool, 0)
-    for _, v := range nums {
-        if _, f := m[v]; f {
+    sort.Ints(nums)
+    n := len(nums)-1
+    for i:=0; i<n; i++{
+        if nums[i] == nums[i+1]{
             return true
-        } else {
-            m[v] = true
-        }
+        } 
     }
-    
     return false
 }
