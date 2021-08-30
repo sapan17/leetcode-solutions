0a37c93fef01d8bbbb5433c00d59a6c1eeb62e5e
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-08-31 00:18:00 +0530

    Time: 287 ms (5.03%), Space: 9 MB (11.65%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.go
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.go
@@ -2,14 +2,14 @@ func maxProfit(prices []int) int {
     if len(prices) < 2{
         return 0
     }
-    minprice := prices[0]
-    maxprofit := 0
+    min := prices[0]
+    max := 0
     for i:=0;i<len(prices);i++{
-        if prices[i]< minprice{
-            minprice = prices[i]
-        } else if prices[i] - minprice > maxprofit{
-            maxprofit = prices[i] - minprice
+        if min > prices[i]{
+            min = prices[i]
+        } else if prices[i] - min > max{
+            max = prices[i] - min
         }
     }
-    return maxprofit
+    return max
 }
