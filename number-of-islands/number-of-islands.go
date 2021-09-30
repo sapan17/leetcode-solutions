5423162bd6faad593bc754203f9c1faa574b3e87
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-09-30 21:57:24 +0530

    Time: 4 ms (62.37%), Space: 3.8 MB (57.14%) - LeetHub

---
+++ number-of-islands/number-of-islands.go
@@ -0,0 +1,37 @@
+var m,n int
+
+func numIslands(board [][]byte) int {
+    m = len(board)
+    if m == 0 {
+        return 0
+    }
+    n = len(board[0])
+    if n == 0 {
+        return 0
+    }
+    
+    res := 0
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if board[i][j] == '1' {
+                res += 1
+                dfs(&board, i, j) // pass the reference of the whole board
+            }
+        }
+    }
+    
+    return res
+    
+}
+
+func dfs(board *[][]byte, i, j int) {
+    if i < 0 || i >= m || j < 0 || j >= n || (*board)[i][j] == '0' {
+        return
+    }
+    
+    (*board)[i][j] = '0' // dereference of the pointer and you will get the original board
+    dfs(board, i+1, j)
+    dfs(board, i, j-1)
+    dfs(board, i, j+1)
+    dfs(board, i-1, j)
+}
