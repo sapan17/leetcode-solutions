e2c9386cb016e0f542054127dc16601fe634bf0f
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-10-01 12:19:03 +0530

    Time: 4 ms (63.44%), Space: 3.8 MB (58.09%) - LeetHub

--- number-of-islands/number-of-islands.go
+++ number-of-islands/number-of-islands.go
@@ -2,14 +2,10 @@ var m,n int
 
 func numIslands(board [][]byte) int {
     m = len(board)
-    if m == 0 {
-        return 0
-    }
     n = len(board[0])
-    if n == 0 {
+    if m == 0 || n == 0 {
         return 0
     }
-    
     res := 0
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
