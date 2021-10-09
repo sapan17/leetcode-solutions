7d47b893aa0cc01077386390db5ffcd55932d0f9
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-10-10 05:06:35 +0530

    Time: 16 ms (88.52%), Space: 6.3 MB (99.59%) - LeetHub

--- missing-number/missing-number.go
+++ missing-number/missing-number.go
@@ -2,8 +2,8 @@ func missingNumber(nums []int) int {
     n := len(nums)
     c := (n*(n+1))/2
     e := 0
-    for _, v:= range nums{
-        e += v
+    for i:=0; i<len(nums); i++{
+        e += nums[i]
     }
     return c - e
 }
