295e3aaf0e178a637d2c3639e64a4bdec272fcb8
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-10-14 07:03:19 +0530

    Time: 4 ms (70.27%), Space: 2.8 MB (89.83%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
@@ -1,34 +1,23 @@
 func intersect(nums1 []int, nums2 []int) []int {
-    p1 := 0
-    p2 := 0
-    res := make([]int,0)
-    cache := make(map[int]struct{},0)
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+
+	var (
+		i,j int
+		ret = []int{}
+	)
+
+	for i, j = 0, 0; i < len(nums1) && j < len(nums2); {
+		if nums1[i] == nums2[j] {
+			ret = append(ret, nums1[i])
+			i++
+			j++
+        } else if nums1[i] < nums2[j] {
+            i++
+        } else{
+            j++
+        }
+	}
     
-    for{
-        if p1 >= len(nums1){
-            break
-        }
-        if p2 >= len(nums2){
-            p2 = 0
-            p1++
-            continue
-        }
-        if nums1[p1] != nums2[p2]{
-            p2++
-            continue
-        }
-        if nums1[p1] == nums2[p2]{
-            _,ok := cache[p2]
-            if ok{
-                p2++
-                continue
-            } else{
-                cache[p2] = struct {}{}
-                res = append(res,nums1[p1])
-                p2 = 0
-                p1++
-            }
-        }
-    }
-    return res
+	return ret
 }
