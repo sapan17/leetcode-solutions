58d0d49398e1ed25e83ff0d0816ddf4a4196cce0
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-10-24 14:14:54 -0700

    Time: 2 ms (32.12%), Space: 2.1 MB (59.38%) - LeetHub

--- spiral-matrix/spiral-matrix.go
+++ spiral-matrix/spiral-matrix.go
@@ -1,20 +1,22 @@
 func spiralOrder(matrix [][]int) []int {
     var res []int
-    n := len(matrix)
-    m := len(matrix[0])
-    left,right,up,down := 0, m-1, 0, n-1
-    for len(res) < m*n{
-        for i:=left; i<=right && len(res)<n*m; i++{
-            res = append(res,matrix[up][i])
+    r := len(matrix[0])
+    c := len(matrix)
+    
+    left, right, up, down := 0, r -1, 0, c-1
+    
+    for len(res) < r*c{
+        for i:=left; i<= right && len(res) < r*c; i++{
+            res = append(res, matrix[up][i])
         }
-        for j:=up+1; j<=down-1 && len(res)<n*m; j++{
-            res = append(res,matrix[j][right])
+        for j:=up+1; j<= down-1 && len(res) < r*c; j++{
+            res = append(res, matrix[j][right])
         }
-        for i:=right; i>=left && len(res)<n*m; i--{
-            res = append(res,matrix[down][i])
+        for i:= right; i>= left && len(res) < r*c; i--{
+            res = append(res, matrix[down][i])
         }
-        for j:=down-1; j>=up+1 && len(res)<n*m; j--{
-            res = append(res,matrix[j][left])
+        for j := down-1; j>=up+1 && len(res) < r*c; j--{
+            res = append(res, matrix[j][left])
         }
         left++
         right--
