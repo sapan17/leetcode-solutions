798bdea816e147258f8243ef0d79452c4e99dd38
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-10-25 20:38:09 -0700

    Time: 67 ms (6.12%), Space: 8.5 MB (21.61%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.go
+++ product-of-array-except-self/product-of-array-except-self.go
@@ -1,18 +1,21 @@
 func productExceptSelf(nums []int) []int {
     n := len(nums)
-    res1 := make([]int, n)
-    res2 := make([]int, n)
-    res3 := make([]int, n)
-    res1[0] = 1
-    res2[n-1] = 1
-    for i := 1; i < n; i++ {
-        res1[i] = res1[i-1] * nums[i-1]
+    
+    arr1 := make([]int, n)
+    arr2 := make([]int, n)
+    arr3 := make([]int, n)
+    
+    arr1[0] = 1
+    arr2[n-1] = 1
+    
+    for i:=1; i<n; i++{
+        arr1[i] = arr1[i-1] * nums[i-1]
     }
-    for i:= n-2; i >= 0; i--{
-        res2[i] = res2[i+1] * nums[i+1]
+    for i:= n-2; i>=0; i--{
+        arr2[i] = arr2[i+1] * nums[i+1]
     }
-    for i := 0; i < n; i++ {
-        res3[i] = res1[i]*res2[i]
+    for i:=0; i<n; i++{
+        arr3[i] = arr1[i] * arr2[i]
     }
-    return res3
+    return arr3
 }
