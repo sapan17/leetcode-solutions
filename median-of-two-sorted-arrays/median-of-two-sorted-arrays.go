1a1cc5cb114c7576ed2f05b13c8f22b5317dd871
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-10-25 21:37:38 -0700

    Time: 12 ms (86.67%), Space: 6.5 MB (24.73%) - LeetHub

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -1,17 +1,17 @@
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-    var nums3 []int
-    for i:=0; i<len(nums1); i++{
-        nums3 = append(nums3,nums1[i])
-    }
+    //var nums3 []int
     for i:=0; i<len(nums2); i++{
-        nums3 = append(nums3,nums2[i])
+        nums1 = append(nums1,nums2[i])
     }
+    //for i:=0; i<len(nums2); i++{
+      //  nums3 = append(nums3,nums2[i])
+    //}
     //nums1 = append(nums1,nums2...)
-    sort.Ints(nums3)
+    sort.Ints(nums1)
     
-    if len(nums3)%2 == 0{
-        return float64(nums3[len(nums3)/2 - 1] + nums3[len(nums3)/2])/2
+    if len(nums1)%2 == 0{
+        return float64(nums1[len(nums1)/2 - 1] + nums1[len(nums1)/2])/2
     } else{
-        return float64(nums3[len(nums3)/2])
+        return float64(nums1[len(nums1)/2])
     }
 }
