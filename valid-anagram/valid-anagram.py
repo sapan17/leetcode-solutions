165f7c801aebcc159aa28c5e368e666ad3e3d89b
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-10-27 22:27:15 -0700

    Time: 64 ms (39.11%), Space: 14.5 MB (56.84%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,13 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        arr1 = [0] * 26
-        arr2 = [0] * 26
-        
+        count = [0]*26
         if len(s) != len(t):
             return False
-        
         for i in range(len(s)):
-            arr1[ord(s[i]) - 97] += 1
-            arr2[ord(t[i]) - 97] += 1
-            
-        return arr1 == arr2
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for i in range(26):
+            if count[i] != 0:
+                return False
+        return True
