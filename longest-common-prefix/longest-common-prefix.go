4362ce4d4b9736625ad1d6df425de6d463553588
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-10-27 20:22:21 -0700

    Time: 0 ms (100.00%), Space: 2.4 MB (100.00%) - LeetHub

--- longest-common-prefix/longest-common-prefix.go
+++ longest-common-prefix/longest-common-prefix.go
@@ -1,7 +1,8 @@
 func longestCommonPrefix(strs []string) string {
+    
     for i:=0; i<len(strs[0]); i++{
         for j:=0; j<len(strs); j++{
-            if len(strs[j])<= i || strs[j][i] != strs[0][i]{
+            if i >= len(strs[j]) || strs[j][i] != strs[0][i]{
                 return strs[0][:i]
             }
         }
