34828c45f9f4ff49fe8343d3a9770a21c18cf919
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2021-11-10 17:39:52 -0800

    Time: 12 ms (95.20%), Space: 13.4 MB (36.64%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b = 1, 1
+        for i in range(n):
+            a, b = a+b, a
+        return b
