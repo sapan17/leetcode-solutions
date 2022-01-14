31d6e619283bf40b3f8f9691d9efb92dd72ddfdc
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-13 19:02:25 -0700

    Time: 796 ms (97.18%), Space: 23 MB (5.80%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -4,13 +4,12 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        
         minprice = prices[0]
         maxprofit = 0
         
         for i in prices:
             if i < minprice:
                 minprice = i
-            if maxprofit < i - minprice:
+            if i - minprice > maxprofit:
                 maxprofit = i - minprice
         return maxprofit
