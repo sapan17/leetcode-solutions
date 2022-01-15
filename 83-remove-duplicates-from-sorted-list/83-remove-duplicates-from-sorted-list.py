0be1c42a2589832c80bd88dd7340cc15c8c85ea4
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-15 09:26:56 -0700

    Time: 28 ms (83.80%), Space: 13.4 MB (89.14%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.py
@@ -10,6 +10,7 @@ class Solution(object):
         :rtype: ListNode
         """
         cur = head
+        
         while cur:
             while cur.next and cur.next.val == cur.val:
                 cur.next = cur.next.next
