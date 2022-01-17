b7b85a2773475a487baa46257fffd6f0bcde4ec4
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-17 10:16:51 -0700

    Time: 168 ms (34.03%), Space: 13.8 MB (71.64%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count = {}
-        l = res = 0
         
+        l = res = 0
+        count = {}
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
             if (r-l+1) - max(count.values()) > k:
                 count[s[l]] -= 1
                 l += 1
-            res = max(res, r-l+1)
+            res = max(res, (r-l+1))
         return res
