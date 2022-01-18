2d1f730902bfcc7a6dc8f8375fae9796fc8b2184
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-18 09:25:28 -0700

    Time: 255 ms, Memory: 14.4 MB - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.py
+++ set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,18 +4,16 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        R = len(matrix)
-        C = len(matrix[0])
-        rows = set()
-        cols = set()
         
-        for i in range(R):
-            for j in range(C):
+        row, col = set(), set()
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    rows.add(i)
-                    cols.add(j)
-                    
-        for i in range(R):
-            for j in range(C):
-                if i in rows or j in cols:
+                    row.add(i)
+                    col.add(j)
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in row or j in col:
                     matrix[i][j] = 0
