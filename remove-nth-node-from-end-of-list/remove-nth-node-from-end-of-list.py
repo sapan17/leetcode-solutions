9c4c0d8a790c23a80025dfe921f747c5e1eb77d5
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-17 23:45:54 -0700

    Time: 44 ms, Memory: 13.2 MB - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -15,8 +15,8 @@ class Solution(object):
         for i in range(n):
             fast = fast.next
         if not fast:
-            return head.next    
-        while fast.next:
+            return head.next
+        while fast and fast.next:
             fast = fast.next
             slow = slow.next
         
