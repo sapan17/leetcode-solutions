b29506c6724db76f31960610e527cfe3346281af
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-21 00:07:41 -0700

    Time: 112 ms, Memory: 19.7 MB - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,8 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
         res = defaultdict(list)
-        
         for s in strs:
             count = [0]*26
             for c in s:
