cbbce1c4b9f3742810df28709e7c409011ace8b6
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-20 22:55:46 -0700

    Time: 24 ms, Memory: 13.5 MB - LeetHub

--- rotate-image/rotate-image.py
+++ rotate-image/rotate-image.py
@@ -6,15 +6,14 @@ class Solution(object):
         """
         self.transpose(matrix)
         self.reflect(matrix)
-    
     def transpose(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i+1,n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j] 
+        
+        for i in range(len(matrix)):
+            for j in range(i,len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     
     def reflect(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(n//2):
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix)//2):
                 matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
