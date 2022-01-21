443af0d8d25eaccd6950aece7a599c1817558170
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-21 10:13:10 -0700

    Time: 144 ms, Memory: 14.8 MB - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -5,6 +5,7 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         i = 0
+        
         for j in range(len(nums)):
             if nums[j] != 0:
                 nums[j], nums[i] = nums[i], nums[j]
