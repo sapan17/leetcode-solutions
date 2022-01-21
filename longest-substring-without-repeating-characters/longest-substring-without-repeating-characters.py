93203ad0a74215746fbd355650764ef4432837d0
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-21 00:12:58 -0700

    Time: 57 ms, Memory: 14.5 MB - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -6,12 +6,10 @@ class Solution(object):
         """
         l = res = 0
         charset = set()
-        
         for r in range(len(s)):
             while s[r] in charset:
                 charset.remove(s[l])
                 l += 1
             charset.add(s[r])
-            res = max(res, r-l+1)
+            res = max(r-l+1, res)
         return res
-            
