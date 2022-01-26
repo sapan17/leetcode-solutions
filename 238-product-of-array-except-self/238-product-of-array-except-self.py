5e67f5e7e05cc0fb52747028e1090a3e93167690
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-26 16:53:12 -0700

    Time: 349 ms (17.46%), Space: 21.9 MB (35.94%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -4,18 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        nums1 = [1]*len(nums)
+        nums2 = [1]*len(nums)
         
-        n = len(nums)
-        nums1 = [1] * n
-        nums2 = [1] * n
+        for i in range(1, len(nums)):
+            nums1[i] = nums[i-1] * nums1[i-1]
         
-        for i in range(1, n):
-            nums1[i] = nums1[i-1] * nums[i-1]
-        
-        for i in range(n-2, -1, -1):
+        for i in range(len(nums)-2, -1, -1):
             nums2[i] = nums[i+1] * nums2[i+1]
             
-        for i in range(n):
-            nums2[i] = nums1[i]*nums2[i]
-            
+        for i in range(len(nums)):
+            nums2[i] = nums1[i] * nums2[i]
+        
         return nums2
