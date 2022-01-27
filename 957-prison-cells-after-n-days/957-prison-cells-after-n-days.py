6615fa80313a106d89325110a4812f563355f786
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-26 23:19:28 -0700

    Time: 43 ms (32.43%), Space: 13.4 MB (47.30%) - LeetHub

---
+++ 957-prison-cells-after-n-days/957-prison-cells-after-n-days.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def prisonAfterNDays(self, cells, n):
+        """
+        :type cells: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        n = (n-1)%14+1
+        while n:
+            temp = [0]*8
+            for i in range(1, 7):
+                if cells[i-1] == cells[i+1]:
+                    temp[i] = 1
+                else:
+                    temp[i] = 0
+            cells = temp
+            n -= 1
+        return cells    
+            
