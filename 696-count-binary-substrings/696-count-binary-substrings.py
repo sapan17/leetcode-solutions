7e7f233dbbcccf1157fdd123689b6f4b9b745a30
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-27 11:17:08 -0700

    Time: 297 ms (14.63%), Space: 15.6 MB (59.31%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.py
+++ 696-count-binary-substrings/696-count-binary-substrings.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: int
         """
         groups = [1]
-        for i in xrange(1, len(s)):
+        for i in range(1, len(s)):
             if s[i-1] != s[i]:
                 groups.append(1)
             else:
                 groups[-1] += 1
                 
         ans = 0
-        for i in xrange(1, len(groups)):
+        for i in range(1, len(groups)):
             ans += min(groups[i-1], groups[i])
         return ans
