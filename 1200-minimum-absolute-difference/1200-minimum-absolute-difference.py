302c7eb8b892a2e12cba314218e59af2de5f458a
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-01-27 18:58:25 -0700

    Time: 385 ms (21.72%), Space: 23.8 MB (75.77%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[List[int]]
         """
-        
         arr.sort()
-        min_diff = float("inf")
-        ans = []
+        min_diff = float('inf')
+        res = []
         for i in range(len(arr)-1):
             if arr[i+1] - arr[i] < min_diff:
                 min_diff = arr[i+1] - arr[i]
         
         for i in range(len(arr)-1):
             if arr[i+1] - arr[i] == min_diff:
-                ans.append([arr[i], arr[i+1]])
+                res.append([arr[i],arr[i+1]])
         
-        return ans
+        return res
+        
