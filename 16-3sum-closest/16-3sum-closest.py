4cc3a3e5f9ce78e6aa0f7d74b6f3b0e48a427468
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-02-05 10:24:18 -0700

    Time: 240 ms (25.22%), Space: 13.6 MB (22.20%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -6,20 +6,22 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()
-        diff = float("inf")
-        for i in range(len(nums)):
-            lo, high = i+1, len(nums)-1
-            while lo < high:
-                threesum = nums[i] + nums[lo] + nums[high]
+        diff = float('inf')
+        for i in range(len(nums)-1):
+            lo, hi = i+1, len(nums)-1
+            while lo < hi:
+                threesum = nums[i] + nums[lo] + nums[hi]
+                
                 if abs(target-threesum) < abs(diff):
                     diff = target - threesum
+
                 if threesum > target:
-                    high -= 1
-                else: 
+                    hi -= 1
+
+                if threesum < target:
                     lo += 1
-            if diff == 0:
-                break
-        return target - diff 
-                
-            
-            
+
+                if threesum == target:
+                    break
+        return target - diff
+        
