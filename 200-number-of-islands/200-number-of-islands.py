f522bf9f1c8326ad437009feb58a19cc3e4a8aa4
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-02-06 13:15:43 -0700

    Time: 329 ms (63.47%), Space: 28.8 MB (72.75%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
+        
         res = 0
         
         for i in range(len(grid)):
@@ -12,7 +13,6 @@ class Solution(object):
                     res += 1
                     self.dfs(grid, i, j)
         return res
-                    
     def dfs(self, grid, i, j):
         if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or grid[i][j] != "1":
             return
