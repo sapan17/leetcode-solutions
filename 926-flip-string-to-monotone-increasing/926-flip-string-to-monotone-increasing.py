28635f422f0c119c45055db493768faaad23fc34
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-02-07 10:45:52 -0700

    Time: 196 ms (78.35%), Space: 18.2 MB (32.73%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -7,10 +7,10 @@ class Solution(object):
         n = len(s)
         count1, count0 = 0, 0
         for i in range(n):
-            if s[i] == "0":
-                count0 += 1
-            else:
+            if s[i] == "1":
                 count1 += 1
+            else:
+                count0 += 1
             count0 = min(count0, count1)
         return count0
             
