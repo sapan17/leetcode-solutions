f0dd9a07c9babb44112ff8807d1de0a27d3596f5
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-02-08 09:20:22 -0700

    Time: 60 ms (13.10%), Space: 13.7 MB (38.73%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -7,14 +7,12 @@ class Solution(object):
         """
         
         normal = ''.join([c.lower() if  c.isalnum() else ' ' for c in paragraph])
-        
-        
+                
         words = normal.split()
         word_count = defaultdict(int)
         banned_words = set(banned)
-        
         for word in words:
-            if word not in banned_words:
+            if word not in banned:
                 word_count[word] += 1
                 
         return max(word_count.items(), key=operator.itemgetter(1))[0]
