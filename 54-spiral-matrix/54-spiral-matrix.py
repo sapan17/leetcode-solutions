b903a3c17975acc191152e86e7f889401518a62b
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-02-07 21:40:55 -0700

    Time: 12 ms (97.25%), Space: 13.3 MB (91.20%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -4,10 +4,9 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        left, right = 0, len(matrix[0])
         top, bottom = 0, len(matrix)
+        left, right = 0, len(matrix[0])
         res = []
-        
         while left < right and top < bottom:
             for i in range(left, right):
                 res.append(matrix[top][i])
@@ -17,15 +16,15 @@ class Solution(object):
                 res.append(matrix[i][right-1])
             right -= 1
             
-            if not (left < right and top < bottom):
+            if not(left < right and top < bottom):
                 break
             
-            for i in range(right-1, left-1,-1):
+            for i in range(right-1, left, -1):
                 res.append(matrix[bottom-1][i])
             bottom -= 1
             
-            for i in range(bottom-1, top-1, -1):
+            for i in range(bottom, top-1, -1):
                 res.append(matrix[i][left])
             left += 1
-        
+            
         return res
