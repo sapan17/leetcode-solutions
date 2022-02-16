ca03583b5bf63e91a7c3b6d4a1297a8197b91922
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-02-15 19:54:55 -0700

    Time: 20 ms (88.13%), Space: 13.2 MB (98.98%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -5,16 +5,14 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         self.transpose(matrix)
-        self.reflection(matrix)
+        self.reflect(matrix)
         
     def transpose(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i+1, n):
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix[0])):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    def reflection(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(n/2):
-                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
+    def reflect(self, matrix):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])//2):
+                matrix[i][j] , matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
