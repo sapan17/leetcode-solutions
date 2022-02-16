f38df44429bc4e856290fee277c69e770481e3cd
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-02-15 20:45:53 -0700

    Time: 60 ms (83.64%), Space: 13.5 MB (71.88%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -10,19 +10,20 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        result = ListNode(0)
-        result_tail = result
+        res = ListNode(0)
+        result = res
         carry = 0
         
         while l1 or l2 or carry:
-            val1 = (l1.val if l1 else 0)
-            val2 = (l2.val if l2 else 0)
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
             carry, out = divmod(val1 + val2 + carry, 10)
             
-            result_tail.next = ListNode(out)
-            result_tail = result_tail.next
+            result.next = ListNode(out)
+            result = result.next
             
-            l1 = (l1.next if l1 else None)
-            l2 = (l2.next if l2 else None)
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
             
-        return result.next
+        return res.next
+            
