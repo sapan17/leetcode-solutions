03d81d6069a8a1b3dd8fcbf4cc82fd290c722dda
Sapan Desai <35861214+sapanpdesai@users.noreply.github.com>
2022-02-17 09:16:43 -0700

    Time: 62 ms (13.15%), Space: 13.7 MB (31.41%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
-        dp = [False]*(len(s)+1)
+        
+        dp = [False] * (len(s)+1)
         dp[0] = True
         
         for i in range(1, len(s)+1):
             for j in range(i):
                 if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
-                    break
+                
         return dp[-1]
-                    
